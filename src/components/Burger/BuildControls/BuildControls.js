7a88c55fad5e6e0be17ad1ce41b8d38aba87f9da
Dale Corns <codefellow@gmail.com>
2018-05-24 15:52:19 -0700

    style order now button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,10 +13,12 @@ const controls = [
   {label: 'Cheese', type: 'cheese', max:4},
   {label: 'Meat', type: 'meat', max:4},
 ];
+const CAN_PURCHASE_PRICE = 1.1;
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
     <p><strong>Total ${props.totalPrice.toFixed(2)}</strong></p>
     {controls.map(ctrl => <BuildControl key={ctrl.label} label={ctrl.label} adjusted={(e) => props.ingredientAdjusted(ctrl.type,parseInt(e.target.dataset.amt,10))} disableMore={props.ingredientTotals[ctrl.type] === ctrl.max} disableLess={props.ingredientTotals[ctrl.type] === 0}/>)}
-  </div>
+  <button className={classes.OrderButton} disabled={props.totalPrice < CAN_PURCHASE_PRICE}>Order Now</button>
+    </div>
 );
 export default buildControls;
